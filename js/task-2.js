dae63d7a1f29be9725047e3a1d465ff6da52426b
Denys Sherstiuk <denissherstjuk@gmail.com>
2024-05-13 16:20:18 +0300

    do tasks

---
+++ js/task-2.js
@@ -0,0 +1,51 @@
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  },
+];
+
+const ul = document.querySelector("ul");
+
+ul.style.display = "flex";
+ul.style.justifyContent = "center";
+ul.style.alignItems = "center";
+ul.style.listStyleType = "none";
+ul.style.flexWrap = "wrap";
+ul.style.gap = "48px 24px";
+ul.style.width = "1128px";
+ul.style.minHeight = "648px";
+ul.style.padding = "0";
+ul.style.margin = "0 auto";
+
+const gallery = images.forEach((image) => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  ul.append(li);
+  li.append(img);
+  li.style.height = "300px";
+  img.style.width = "360px";
+  img.style.height = "300px";
+  img.src = image.url;
+  img.alt = image.alt;
+});
---
+++ js/task-6.js
@@ -0,0 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const quantity = document.querySelector("input");
+const box = document.querySelector("#boxes");
+
+create.addEventListener("click", createBoxes);
+destroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  box.innerHTML = "";
+
+  amount = quantity.value;
+  if (amount > 0 && amount <= 100) {
+    for (let i = 1; i <= amount; i++) {
+      const divBox = document.createElement("div");
+      box.append(divBox);
+      divBox.style.width = `${20 + i * 10}px`;
+      divBox.style.height = `${20 + i * 10}px`;
+      divBox.style.backgroundColor = getRandomHexColor();
+    }
+  }
+  quantity.value = "";
+}
+
+function destroyBoxes() {
+  box.innerHTML = "";
+}
